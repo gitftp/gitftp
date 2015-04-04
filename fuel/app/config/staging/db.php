3a1af49b0644b3fb0b97cbb6414053725d97f90b
Boniface <Boniface@boniface-pc>
2015-04-05 00:28:34 +0530

    test

--- fuel/app/config/staging/db.php
+++ fuel/app/config/staging/db.php
@@ -6,7 +6,7 @@
 return array(
     'default' => array(
         'connection' => array(
-            'dsn' => 'mysql:host=localhost;dbname=craftrzt_gitploy',
+            'dsn' => 'mysql:host=pc.craftpip.com;dbname=craftrzt_gitploy',
             'username' => 'root',
             'password' => 'thisissparta',
         ),

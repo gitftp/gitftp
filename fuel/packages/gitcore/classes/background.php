dbb0a3aa7903bbf37e89ad32100014f5286a275b
Boniface <Boniface@boniface-pc>
2015-04-07 11:46:23 +0530

    a

--- fuel/packages/gitcore/classes/background.php
+++ fuel/packages/gitcore/classes/background.php
@@ -1,9 +1,15 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-echo 'asda';
+class background {
+
+    static function init() {
+        echo 'asdadas';
+    }
+
+}

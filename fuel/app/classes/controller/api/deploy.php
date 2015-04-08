26942a108390338d09eb7bd70e2339794563ab2c
Boniface <Boniface@boniface-pc>
2015-04-08 13:10:34 +0530

    asd

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -43,13 +43,17 @@ class Controller_Api_Deploy extends Controller {
             ));
         }
 
+        $user_dir = DOCROOT . 'fuel/repository/' . $user_id;
         $repo_dir = DOCROOT . 'fuel/repository/' . $user_id . '/' . $b[0]['name'];
         chdir($repo_dir);
         echo shell_exec('chown www-data * -R');
         echo shell_exec('chgrp www-data * -R');
         echo shell_exec('chmod 777 -R');
+        echo shell_exec('rm .git/ -R');
+        echo shell_exec('rm * -R');
+        echo shell_exec('rm ' . $b[0]['name'] . ' -R');
 
-        File::delete_dir($repo_dir, true, true);
+//        File::delete_dir($repo_dir, true, true);
 
         if (count($b) != 0) {
             DB::delete('deploy')->where('id', $id)->execute();

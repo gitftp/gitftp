16e18cec533889c6c65be7c91b720dfa3f9e05e0
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 02:21:44 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -24,5 +24,7 @@ class Controller_Hook extends Controller {
     public function action_get(){
         echo '<pre>';
         $a = DB::select()->from('test')->->execute()->as_array();
+        
+        print_r(unserialize($a[1]));
     }
 }

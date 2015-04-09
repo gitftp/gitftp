3b243fca4cfe6f25108c923ca3d3aa13851b82cf
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 02:46:35 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -24,8 +24,8 @@ class Controller_Hook extends Controller {
         }
 
         $i = $_REQUEST['payload'];
-        
-        echo $i->ref;
+        echo '<pre>';
+        print_r($i);
 //        $record = new Model_Record();
 //        $record_id = $record->insert(array(
 //            'deploy_id' => $deploy_id,

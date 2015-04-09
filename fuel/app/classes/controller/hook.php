74c6550fe967773863dfc343ca7ea2f29c5ef434
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 02:55:53 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -29,7 +29,7 @@ class Controller_Hook extends Controller {
         $record = new Model_Record();
         $record_id = $record->insert(array(
             'deploy_id' => $deploy_id,
-            'user_id' => $user_id,
+            'user_id' => (int) $user_id,
             'status' => 2,
             'date' => time(),
             'triggerby' => $i->pusher->name,

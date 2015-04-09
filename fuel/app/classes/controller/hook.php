832a16e089fb4c7e9aba678a8f4c54671c58407e
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 03:03:01 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -41,7 +41,7 @@ class Controller_Hook extends Controller {
                 ))->execute();
 
         $repo_dir = DOCROOT . 'fuel/repository/' . $user_id . '/' . $deploy_id;
-        
+
         DB::insert('test')->set(array(
             'test' => serialize($_REQUEST['payload'])
         ))->execute();

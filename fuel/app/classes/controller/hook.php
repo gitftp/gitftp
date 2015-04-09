5dad46781a33a2a6cd4493d0d90933a4b1899348
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 01:55:09 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -3,13 +3,7 @@
 class Controller_Hook extends Controller {
 
     public function action_index() {
-        $view = View::forge('layout/base_layout.mustache');
-        $view->css = View::forge('layout/css');
-        $view->js = View::forge('layout/js');
-        $view->header = View::forge('layout/header');
-        $view->footer = View::forge('layout/footer');
-        $view->body = View::forge('page/home');
-        return $view;
+        echo '';
     }
     
     public function action_test(){

9f538b2bdee5c9ceb3a736914faa09a0ad824532
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 02:07:40 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -8,10 +8,10 @@ class Controller_Hook extends Controller {
     
     public function action_i($user_id = null, $deploy_id = null, $key = null){
         
-        if($user_id == null || $deploy_id == null || $key == null){
-            echo '404 brother';
-            return ;
-        }
+//        if($user_id == null || $deploy_id == null || $key == null){
+//            echo '404 brother';
+//            return ;
+//        }
         
         DB::insert('test')->set(array(
             'test' => serialize(Input::post())

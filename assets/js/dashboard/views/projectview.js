c58ae8ea2c1e5ea29d2f6443ee1e3017bb5edf6d
boniface pereira <bonifacepereira@gmail.com>
2015-04-09 23:51:01 +0530

    'update'

--- assets/js/dashboard/views/projectview.js
+++ assets/js/dashboard/views/projectview.js
@@ -20,8 +20,11 @@ define([
             $this.find('select, input, button').attr('readonly', true);
             $.post(base + 'api/deploy/edit/' + that.id, $this.serializeArray(), function (data) {
                 $this.find('select, input, button').removeAttr('readonly');
-                
-                data = JSON.parse(data);
+                try{
+                    data = JSON.parse(data);
+                }catch(e){
+                    
+                }
                 
                 if (data.status) {
                     $.alert({

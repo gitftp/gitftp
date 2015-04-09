cb606f5e90968bd22653e87cc6362eec0e19a801
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 02:12:19 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -14,7 +14,7 @@ class Controller_Hook extends Controller {
 //        }
         
         DB::insert('test')->set(array(
-            'test' => serialize(Input::post())
+            'test' => serialize(Input::post()).$HTTP_RAW_POST_DATA
         ))->execute();
 //        if(Input::method() != 'POST'){
 //            

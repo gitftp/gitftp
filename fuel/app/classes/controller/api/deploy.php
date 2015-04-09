7df1ba100fa81ad10992666a911df650716e94a0
boniface pereira <bonifacepereira@gmail.com>
2015-04-09 23:55:48 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -191,7 +191,7 @@ class Controller_Api_Deploy extends Controller {
          */
         $record_id = $record->insert(array(
             'deploy_id' => $repo['id'],
-            'status' => 2,
+            'status' => 2, // working
             'triggerby' => 'user',
             'date' => time(),
         ));

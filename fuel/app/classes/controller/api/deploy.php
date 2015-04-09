f118118daf773812a843e0ce57a0942f210de420
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 00:14:02 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -299,7 +299,6 @@ class Controller_Api_Deploy extends Controller {
             'status' => 1,
             'amount_deployed' => $log['gitftpop']['deployed']['human'],
             'deployed_bytes' => $log['gitftpop']['deployed']['data'],
-            
         ));
 
         $ftp_data = $repo['ftpdata'];

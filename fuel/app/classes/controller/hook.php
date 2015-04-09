f59de663ee5c07dbb4ea6b3062bb650a3ac8c2d5
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 02:33:19 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -5,17 +5,17 @@ class Controller_Hook extends Controller {
     public function action_index() {
         echo '';
     }
-    
-    public function action_i($user_id = null, $deploy_id = null, $key = null){
-        
-        if($user_id == null || $deploy_id == null || $key == null || Input::method() != 'POST'){
+
+    public function action_i($user_id = null, $deploy_id = null, $key = null) {
+
+        if ($user_id == null || $deploy_id == null || $key == null || Input::method() != 'POST') {
             echo 'Something is missing';
-            return ;
+            return;
         }
-        
+
         $check = DB::select()
-        
-        
+
+
         $record = new Model_Record();
         $record_id = $record->insert(array(
             'deploy_id' => $deploy_id,
@@ -25,15 +25,16 @@ class Controller_Hook extends Controller {
             'triggerby' => 'GitHub',
             'post_data' => serialize($_REQUEST['payload']),
         ));
-        
+
         DB::insert('test')->set(array(
             'test' => serialize($_REQUEST['payload'])
         ))->execute();
     }
-    
-    public function action_get(){
+
+    public function action_get() {
         echo '<pre>';
         $a = DB::select()->from('test')->execute()->as_array();
         print_r(json_decode(unserialize($a[1]['test'])));
     }
+
 }

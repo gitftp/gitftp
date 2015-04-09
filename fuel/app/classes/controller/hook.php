6a7b2465115e44fcc592b9d3bb10b879f5ced1de
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 01:54:56 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -1,8 +1,18 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Controller_Welcome extends Controller {
 
+    public function action_index() {
+        $view = View::forge('layout/base_layout.mustache');
+        $view->css = View::forge('layout/css');
+        $view->js = View::forge('layout/js');
+        $view->header = View::forge('layout/header');
+        $view->footer = View::forge('layout/footer');
+        $view->body = View::forge('page/home');
+        return $view;
+    }
+    
+    public function action_test(){
+        Debug::constants();
+    }
+}

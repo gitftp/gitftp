33ef21799fedafd14d966169ac1e1366f4aa8a50
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 01:57:25 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -6,7 +6,9 @@ class Controller_Hook extends Controller {
         echo '';
     }
     
-    public function action_i($user_id, $deploy_id, $key = null){
+    public function action_i($user_id = null, $deploy_id = null, $key = null){
+        
+        
         
     }
 }

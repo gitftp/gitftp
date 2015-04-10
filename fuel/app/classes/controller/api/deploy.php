f7f38730ae92e1abeadde99705e64dc2a30f5c38
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 23:47:28 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -229,7 +229,7 @@ class Controller_Api_Deploy extends Controller {
         $repodir = $userdir . '/' . $repo['id'];
 
         chdir($userdir);
-
+        
         exec('git clone --depth 1 ' . $repo['repository'] . ' ' . $repo['id'], $gitcloneop);
         
         $a = File::read_dir($repodir);

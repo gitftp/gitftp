fa06027fc15876d7c3328a5369ffd15ce911e728
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 00:27:19 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -224,9 +224,10 @@ class Controller_Api_Deploy extends Controller {
 
         chdir($userdir);
         
-        
+        //test
                         echo json_encode(array('status' => false, 'reason' => 'There was a problem in connecting to your repository.<br>Please verify the Repository URL. <code>'.$repo['repository'].'</code>'));
 
+        //test
         exec('git clone --depth 1 ' . $repo['repository'] . ' ' . $repo['id'], $gitcloneop);
 
         try {

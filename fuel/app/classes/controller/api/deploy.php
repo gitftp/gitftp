8670f82c617a3b47ceba2e3a375c8355918eea7c
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 01:22:47 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -347,6 +347,7 @@ class Controller_Api_Deploy extends Controller {
             ));
             $deploy->set($id, array(
                 'cloned' => 0,
+                'status' => ''
             ))
             return;
         }

498bbd8e76b647aa8843d017631c7b4e2eda2f3b
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 00:32:52 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -230,7 +230,8 @@ class Controller_Api_Deploy extends Controller {
         $repo_url = parse_url($repo['repository']);
         $repo_url['user'] = 'username';
         $repo_url['pass'] = 'passwrd';
-        http_build_url($repo_url);
+        $repo_url = http_build_url($repo_url);
+        echo $repo_url;
         die();
         //test
         exec('git clone --depth 1 ' . $repo['repository'] . ' ' . $repo['id'], $gitcloneop);

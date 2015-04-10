f580241ef1ad2c84676e3f2b83c8e71bee93b268
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 15:53:31 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -133,7 +133,7 @@ class Controller_Hook extends Controller {
             'deployed' => true,
             'lastdeploy' => date("Y-m-d H:i:s", (new DateTime())->getTimestamp()),
             'ftp' => serialize($ftp),
-            'status' => 'Idasdasdase',
+            'status' => 'Idle',
             'ready' => 1
         ));
     }

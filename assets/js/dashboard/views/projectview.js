44b1c59420e5e3cfbc06cff411f5aad529f0557a
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 22:42:41 +0530

    'update'

--- assets/js/dashboard/views/projectview.js
+++ assets/js/dashboard/views/projectview.js
@@ -42,28 +42,6 @@ define([
                 }
             });
         },
-        process_id: 0,
-        updateView: function (e) {
-            var that = this;
-            if ($('.is-deploy-view-id').length) {
-                var id = $('.is-deploy-view-id').attr('data-id');
-                setTimeout(function () {
-                    that.updateViewProcess(id);
-                }, 1500);
-            } else {
-
-            }
-        },
-        updateViewProcess: function (id) {
-            var that = this;
-
-            $.getJSON(base + 'api/deploy/getall/' + id, function (data) {
-                console.log('here', data);
-                data = data.data[0];
-                $('.project-v-status').html(data.status);
-                that.updateView();
-            });
-        },
         getRawData: function (e) {
             e.preventDefault();
             var $this = $(e.currentTarget);

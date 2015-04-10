3c5f3ebcf2b3178d105bb724f116692f635de566
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 02:22:13 +0530

    'update'

--- assets/js/dashboard/views/ftpadd.js
+++ assets/js/dashboard/views/ftpadd.js
@@ -15,7 +15,7 @@ define([
                 d = JSON.parse(d);
                 $.alert({
                     title: (d.status) ? 'Successful' : 'Falied',
-                    content: (d.status)
+                    content: (d.status) ? ''
                 })
             });
         },

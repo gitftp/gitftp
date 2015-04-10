d73206f45e90e31bf38e94f26495d7a768208567
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 02:00:28 +0530

    'update'

--- fuel/packages/utils/classes/utils.php
+++ fuel/packages/utils/classes/utils.php
@@ -12,6 +12,7 @@ class utils {
 //    [path] => 
 //    [port] => 
 //)
+        
         $b = array(
             'hostname' => $a['host'],
             'username' => $a['username'],
@@ -19,7 +20,7 @@ class utils {
             'timeout' => 120,
             'port' => $a['port'],
             'passive' => true,
-            'ssl_mode' => false,
+            'ssl_mode' => ,
             'debug' => false
         );
         $c = Ftp::forge(array(

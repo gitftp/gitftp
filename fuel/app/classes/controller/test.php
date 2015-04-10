81fc3fd968655ab326c755ec059cc43d00c98647
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 15:06:58 +0530

    'update'

--- fuel/app/classes/controller/test.php
+++ fuel/app/classes/controller/test.php
@@ -53,7 +53,7 @@ class Controller_Test extends Controller {
         } catch (Exception $ex) {
             print_r($ex);
         }
-        
+        echo '--------------';
         print_r($gitcore->log);
         
     }

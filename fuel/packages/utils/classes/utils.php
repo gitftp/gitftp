39376be870f5a83b1b56fafaa1b749a0fc51ba51
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 01:48:21 +0530

    'update'

--- fuel/packages/utils/classes/utils.php
+++ fuel/packages/utils/classes/utils.php
@@ -1,11 +1,22 @@
 <?php
 
 class utils {
-    
-    public static function test_ftp($id){
-        
+
+    public static function test_ftp($a = array()) {
+
+        $c = Ftp::forge(array(
+            'hostname' => 'fuelphp.com',
+            'username' => '',
+            'password' => '',
+            'timeout' => 90,
+            'port' => 21,
+            'passive' => true,
+            'ssl_mode' => false,
+            'debug' => false
+        ));
+
     }
-    
+
 }
 
 /* end of file auth.php */

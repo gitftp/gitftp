1a4dd8d27c3ef342e8c0554d806242a3f073a149
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 19:41:03 +0530

    'update'

--- assets/js/dashboard/views/projectview.js
+++ assets/js/dashboard/views/projectview.js
@@ -46,6 +46,7 @@ define([
             var that = this;
             if ($('.is-deploy-view-id').length) {
                 var id = $('.is-deploy-view-id').attr('data-id');
+                this.process_id = id;
                 
                 this.process = setTimeout(function () {
                     that.updateViewProcess(id);

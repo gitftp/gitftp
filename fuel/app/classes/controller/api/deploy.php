3b440fe0961e3ce6cf8c5d39f07df63588d56f78
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 00:31:07 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -225,6 +225,7 @@ class Controller_Api_Deploy extends Controller {
         chdir($userdir);
         
         //test
+        
         echo json_encode(array('status' => false, 'reason' => 'There was a problem in connecting to your repository.<br>Please verify the Repository URL. <code>'.$repo['repository'].'</code>'));
         print_r(parse_url($repo['repository']));
         die();

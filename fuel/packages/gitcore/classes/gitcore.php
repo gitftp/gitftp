88f4eb403236f84bc3111e859befaf376ad38f62
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 13:16:07 +0530

    'update'

--- fuel/packages/gitcore/classes/gitcore.php
+++ fuel/packages/gitcore/classes/gitcore.php
@@ -805,7 +805,7 @@ class Gitcore {
         try {
             $connection = new bridge($server);
             $this->connection = $connection;
-            $this->log['ftp']
+            $this->log['ftpconnect'] = 'connected'
         } catch (\Exception $e) {
 //            echo Ansi::tagsToColors("\r\n<red>Oh Snap: {$e->getMessage()}\r\n");
             $this->log['ftpconnecterror'] = $e->getMessage();

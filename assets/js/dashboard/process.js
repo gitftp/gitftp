604151cdb1e31f2c7839f2b4c20010d74bac0cca
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 22:43:24 +0530

    'update'

--- assets/js/dashboard/process.js
+++ assets/js/dashboard/process.js
@@ -1,23 +1,13 @@
-
-        process_id: 0,
-        updateView: function (e) {
+define([
+    'text!pages/dashboard.html'
+], function (page) {
+    d = Backbone.View.extend({
+        el: app.el,
+        render: function (id) {
             var that = this;
-            if ($('.is-deploy-view-id').length) {
-                var id = $('.is-deploy-view-id').attr('data-id');
-                setTimeout(function () {
-                    that.updateViewProcess(id);
-                }, 1500);
-            } else {
-
-            }
-        },
-        updateViewProcess: function (id) {
-            var that = this;
-
-            $.getJSON(base + 'api/deploy/getall/' + id, function (data) {
-                console.log('here', data);
-                data = data.data[0];
-                $('.project-v-status').html(data.status);
-                that.updateView();
-            });
-        },
+            that.$el.html(page);
+        }
+    });
+    return d;
+});
+        

2e500279f29ada1468b24e2cec35e6eec99c327e
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 11:24:44 +0530

    'update'

--- fuel/packages/gitcore/classes/gitcore.php
+++ fuel/packages/gitcore/classes/gitcore.php
@@ -863,7 +863,7 @@ class Gitcore {
         
         // If revision is not HEAD, the current one, it means this is a rollback.
         // So, we have to revert the files the the state they were in that revision.
-        if ($this->revision != 'HEAD' && false) {
+        if ($this->revision != 'HEAD') {
             $this->output("   Rolling back working copy");
             $this->log['push'] = 'Rolling back working copy';
 

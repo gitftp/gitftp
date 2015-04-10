ff7cae5c8ef3fb44d6da10a8f38f70713d46b981
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 01:41:59 +0530

    'update'

---
+++ fuel/packages/custom_1/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.5
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+Autoloader::add_classes(array(
+	'idconvert'           => __DIR__.'/classes/idconvert.php',	
+));
+
+
+/* End of file bootstrap.php */

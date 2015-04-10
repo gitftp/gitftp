ff256c02b54455407fee4e413a1bb8ce073f2771
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 01:42:34 +0530

    'update'

--- fuel/packages/utils/classes/utils.php
+++ fuel/packages/utils/classes/utils.php
@@ -1,16 +1,6 @@
 <?php
 
-/**
- * Fuel is a fast, lightweight, community driven PHP5 framework.
- *
- * @package    Fuel
- * @version    1.5
- * @author     Fuel Development Team
- * @license    MIT License
- * @copyright  2010 - 2013 Fuel Development Team
- * @link       http://fuelphp.com
- */
-class idconvert {
+class utils {
 
     public static function get_data_model($where = array()) {
         $data = \Model_CommonFunction::get_data(array('table' => $where['table']));

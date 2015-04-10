33e8ac5cc6d4e5a43257fd1a87597dd04c849ded
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 01:45:52 +0530

    'update'

--- fuel/app/classes/controller/api/ftp.php
+++ fuel/app/classes/controller/api/ftp.php
@@ -30,10 +30,13 @@ class Controller_Api_Ftp extends Controller {
         ));
     }
 
-    public function action_testftp($id){
+    public function action_testftp($id = null){
         if($id == null){
             json_encode(array('status'=>false));
         }
+        
+        
+        
     }
     
     /**

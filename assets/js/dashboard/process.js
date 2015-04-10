609be210fd2576cd8219a38af11dcd81e9e29fdb
boniface pereira <bonifacepereira@gmail.com>
2015-04-10 22:43:18 +0530

    'update'

--- assets/js/dashboard/process.js
+++ assets/js/dashboard/process.js
@@ -1,7 +1,23 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+        process_id: 0,
+        updateView: function (e) {
+            var that = this;
+            if ($('.is-deploy-view-id').length) {
+                var id = $('.is-deploy-view-id').attr('data-id');
+                setTimeout(function () {
+                    that.updateViewProcess(id);
+                }, 1500);
+            } else {
 
+            }
+        },
+        updateViewProcess: function (id) {
+            var that = this;
+
+            $.getJSON(base + 'api/deploy/getall/' + id, function (data) {
+                console.log('here', data);
+                data = data.data[0];
+                $('.project-v-status').html(data.status);
+                that.updateView();
+            });
+        },

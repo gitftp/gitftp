b63a60e69e2ec2f51e5c70ff8f20d4c16115af78
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 20:35:26 +0530

    'update'

--- fuel/app/classes/controller/api/records.php
+++ fuel/app/classes/controller/api/records.php
@@ -21,7 +21,7 @@ class Controller_Api_Records extends Controller {
         foreach ($data as $k => $v) {
             $data[$k]['file_add'] = $this->newLine(unserialize($data[$k]['file_add']));
             $data[$k]['file_remove'] = $this->newLine(unserialize($data[$k]['file_remove']));
-            $data[$k]['file_skip'] = unserialize($data[$k]['file_skip']);
+            $data[$k]['file_skip'] = $this->newLine(unserialize($data[$k]['file_skip']));
         }
 
         echo json_encode(array(

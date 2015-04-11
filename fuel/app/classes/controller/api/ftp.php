4107b1fdea1d1f19747f77a077cfa34c702c630b
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 18:17:48 +0530

    'update'

--- fuel/app/classes/controller/api/ftp.php
+++ fuel/app/classes/controller/api/ftp.php
@@ -132,8 +132,8 @@ class Controller_Api_Ftp extends Controller {
                 ));
             }else{
                 echo json_encode(array(
-                    'status' => false,
-                    'reason' => 'Cannot update with the same values,',
+                    'status' => true,
+                    'reason' => ,
                     'request' => $id
                 ));
             }

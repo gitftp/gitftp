7b51b105653069f00b8b9f5dd33bc4ebddc85646
boniface pereira <bonifacepereira@gmail.com>
2015-04-12 01:20:09 +0530

    'update'

--- fuel/packages/utils/classes/utils.php
+++ fuel/packages/utils/classes/utils.php
@@ -54,7 +54,7 @@ class utils {
         $i = json_decode($input['payload']);
         
         if(isset($i->canon_url)){
-            
+            if()
         }
         
         if($service == 'github'){

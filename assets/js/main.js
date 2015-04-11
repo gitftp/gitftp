4d4d481d724ec89d30547bfedbd23f0622f64d6d
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 18:40:39 +0530

    'update'

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ $(function () {
         return $.ajax(arg)
                 .error(function (data) {
                     switch (data.status) {
-                        case '0':
+                        case 0:
                             $.alert({
                                 title: 'Problem', 
                                 content: 'Servers have gone away, please check if you have a active internet connection.',

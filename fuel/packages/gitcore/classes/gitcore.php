ea3e36632ddd4f3ac9d7abccc6b29a5f96b32861
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 23:08:29 +0530

    'update'

--- fuel/packages/gitcore/classes/gitcore.php
+++ fuel/packages/gitcore/classes/gitcore.php
@@ -854,7 +854,7 @@ class gitcore {
 
             $numberOfFilesToDelete = count($filesToDelete);
             
-            $this->output("going to remove file :".$this->connection->exists($file));
+            $this->output("going to remove file :".$this->connection->exists('kevin - Copy - Copy - Copy.txt'));
             $this->connection->rm($file);
             $this->output("shit");
             $fileNo = str_pad( ++$fileNo, strlen($numberOfFilesToDelete), ' ', STR_PAD_LEFT);

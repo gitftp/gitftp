f7728bddd9503be217236fd58ae8f7b88aaaf20f
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 18:38:57 +0530

    'update'

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ $(function () {
                 .error(function (data) {
                     switch (data.status) {
                         case '0':
-                            $.alert({title: 'Problem', content: 'There was an error connecting, please check if you have a active internet connection.'})
+                            $.alert({title: 'Problem', content: 'Servers are gone away, please check if you have a active internet connection.'})
                             break;
                     }
                 })

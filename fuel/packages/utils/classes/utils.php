6d5cda5709c734312a7f284d092f2205e71f9b4c
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 19:56:49 +0530

    'update'

--- fuel/packages/utils/classes/utils.php
+++ fuel/packages/utils/classes/utils.php
@@ -24,7 +24,7 @@ class utils {
             'debug' => true
         );
         try{
-            $c = Ftp::forge($b);
+            $c = \Fuel\Core\Ftp::forge($b);
         } catch (Exception $ex) {
             return $ex->getMessage();
         }

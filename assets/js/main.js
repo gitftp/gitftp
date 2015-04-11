a488b2a98c0ef7e643e00c5bc01fe3c794207502
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 18:03:03 +0530

    'update'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 
 alert('asda');
-$(function () {
-    if ($('#ftpadd').length) {
-        $('#ftpadd').on('submit', function (e) {
-            e.preventDefault();
-
-            $.post(base + 'api/addftp', $(this).serializeArray(), function (data) {
-                console.log(data);
-            });
-        });
-    }
-});
+//$(function () {
+//    if ($('#ftpadd').length) {
+//        $('#ftpadd').on('submit', function (e) {
+//            e.preventDefault();
+//
+//            $.post(base + 'api/addftp', $(this).serializeArray(), function (data) {
+//                console.log(data);
+//            });
+//        });
+//    }
+//});

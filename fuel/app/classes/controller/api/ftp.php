73d8aa9184a7d3b2ce100f48ee8e77dc966f980f
boniface pereira <bonifacepereira@gmail.com>
2015-04-11 18:13:55 +0530

    'update'

--- fuel/app/classes/controller/api/ftp.php
+++ fuel/app/classes/controller/api/ftp.php
@@ -133,7 +133,7 @@ class Controller_Api_Ftp extends Controller {
             }else{
                 echo json_encode(array(
                     'status' => false,
-                    'request' => 'Could not update FTP config.'
+                    'request' => 'Could not update FTP config. please try again'
                 ));
             }
         }

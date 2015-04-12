454fed291281c309ab948152aecced11631f67a2
boniface pereira <bonifacepereira@gmail.com>
2015-04-12 13:21:22 +0530

    'update'

--- assets/js/dashboard/views/projectview.js
+++ assets/js/dashboard/views/projectview.js
@@ -157,7 +157,7 @@ define([
         renderMoreActivity: function (e) {
             e.preventDefault();
             $this = $(e.currentTarget);
-            $this.html('<i class="fa fa-spin fa-refresh"></i> ')
+            $this.html('<i class="fa fa-spin fa-refresh"></i> Getting data');
             var count = $('tr.activity-data-records').length;
             var that = this;
             _ajax({

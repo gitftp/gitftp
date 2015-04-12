b4c16dddd347bfb6870a40f840c618993bb2a874
boniface pereira <bonifacepereira@gmail.com>
2015-04-12 10:53:56 +0530

    'update'

--- assets/js/dashboard/process.js
+++ assets/js/dashboard/process.js
@@ -3,7 +3,6 @@ define([
 
     var process = {
         process_id: 0,
-        
         deployView: function (e) {
             var that = this;
             /*
@@ -11,22 +10,22 @@ define([
              */
             this.process_id = setTimeout(function () {
                 if ($('.is-deploy-view-id').length) {
-                    
-                }
-                var id = $('.is-deploy-view-id').attr('data-id');
 
-                $.getJSON(base + 'api/deploy/getall/' + id, function (data) {
-                    data = data.data[0];
-                    var el = $('.project-v-status');
-                    if(data.status == 'Idle' || data.status == 'to be initialized'){
-                        el.removeClass("project-co-loading");
-                        el.html(data.status);
-                    }else{
-                        el.addClass("project-co-loading");
-                        el.html('<i class="fa fa-spin fa-refresh fa-fw"></i> '+data.status);
-                    }
-                });
-                that.runProcess();
+                    var id = $('.is-deploy-view-id').attr('data-id');
+
+                    $.getJSON(base + 'api/deploy/getall/' + id, function (data) {
+                        data = data.data[0];
+                        var el = $('.project-v-status');
+                        if (data.status == 'Idle' || data.status == 'to be initialized') {
+                            el.removeClass("project-co-loading");
+                            el.html(data.status);
+                        } else {
+                            el.addClass("project-co-loading");
+                            el.html('<i class="fa fa-spin fa-refresh fa-fw"></i> ' + data.status);
+                        }
+                    });
+                }
+                that.deployView();
             }, 1500);
         },
         runProcess: function () {

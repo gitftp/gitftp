679cd46028d41bfa12b57789a7ce2269e7b5c9b7
boniface pereira <bonifacepereira@gmail.com>
2015-04-12 13:02:25 +0530

    'update'

--- assets/js/dashboard/views/projectview.js
+++ assets/js/dashboard/views/projectview.js
@@ -132,7 +132,7 @@ define([
                     var subPage = that.template[that.which]({
                         's': that.data.data[0],
                         'activity': data,
-                        'more': false
+                        'more': true
                     });
                     $('.deploy-sub-page').html(subPage);
                 });

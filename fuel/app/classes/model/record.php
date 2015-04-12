5d8fab26a8f2f775a6df98f0dc8b20f7fec51e45
boniface pereira <bonifacepereira@gmail.com>
2015-04-12 12:52:39 +0530

    'update'

--- fuel/app/classes/model/record.php
+++ fuel/app/classes/model/record.php
@@ -41,6 +41,8 @@ class Model_Record extends Model {
         $q = DB::select('id')->from($this->table)
                 ->where('user_id', $this->user_id)
                 ->and_where('deploy_id', $id)
+                ->execute()
+                ->as_array();
     }
     
     /**

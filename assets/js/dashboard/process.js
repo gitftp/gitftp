a0221694969256c6403d132aedb433e11b0540af
boniface pereira <bonifacepereira@gmail.com>
2015-04-12 11:08:50 +0530

    'update'

--- assets/js/dashboard/process.js
+++ assets/js/dashboard/process.js
@@ -37,8 +37,8 @@ define([
                     $('.dynamicTime').each(function(i, a){
                         var $this = $(this);
                         var timestamp = parseInt($this.attr('data-timestamp'));
-                        var ts = Math.round((new Date()).getTime() / 1000)-timestamp;
-                        var timeec = (new Date()).getTime()-timestamp;
+                        var timeec = Math.round((new Date()).getTime() / 1000)-timestamp;
+//                        var timeec = (new Date()).getTime()-timestamp;
                         $this.html(moment.duration(timeec).humanize());
                     });
                     

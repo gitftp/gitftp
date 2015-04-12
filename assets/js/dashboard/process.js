4b7f538d2f5df56313c0a63944495d917bb61b8d
boniface pereira <bonifacepereira@gmail.com>
2015-04-12 11:13:59 +0530

    'update'

--- assets/js/dashboard/process.js
+++ assets/js/dashboard/process.js
@@ -37,7 +37,7 @@ define([
                     $('.dynamicTime').each(function(i, a){
                         var $this = $(this);
                         var timestamp = parseInt($this.attr('data-timestamp'));
-                        var diff = parseInt((new Date()).getTime().toString().substr(0, 11))-timestamp;
+                        var diff = parseInt((new Date()).getTime().toString().substr(0, 10))-timestamp;
 //                        var timeec = (new Date()).getTime()-timestamp;
                         $this.html(moment.duration(diff).humanize());
                     });

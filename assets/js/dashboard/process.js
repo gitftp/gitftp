358c8cb0a77f55103d2a372b3b3d4cca0e72a064
boniface pereira <bonifacepereira@gmail.com>
2015-04-12 10:53:38 +0530

    'update'

--- assets/js/dashboard/process.js
+++ assets/js/dashboard/process.js
@@ -9,28 +9,28 @@ define([
             /*
              * Project view page.
              */
-            var id = $('.is-deploy-view-id').attr('data-id');
-
-            $.getJSON(base + 'api/deploy/getall/' + id, function (data) {
-                data = data.data[0];
-                var el = $('.project-v-status');
-                if(data.status == 'Idle' || data.status == 'to be initialized'){
-                    el.removeClass("project-co-loading");
-                    el.html(data.status);
-                }else{
-                    el.addClass("project-co-loading");
-                    el.html('<i class="fa fa-spin fa-refresh fa-fw"></i> '+data.status);
+            this.process_id = setTimeout(function () {
+                if ($('.is-deploy-view-id').length) {
+                    
                 }
-            });
+                var id = $('.is-deploy-view-id').attr('data-id');
+
+                $.getJSON(base + 'api/deploy/getall/' + id, function (data) {
+                    data = data.data[0];
+                    var el = $('.project-v-status');
+                    if(data.status == 'Idle' || data.status == 'to be initialized'){
+                        el.removeClass("project-co-loading");
+                        el.html(data.status);
+                    }else{
+                        el.addClass("project-co-loading");
+                        el.html('<i class="fa fa-spin fa-refresh fa-fw"></i> '+data.status);
+                    }
+                });
+                that.runProcess();
+            }, 1500);
         },
         runProcess: function () {
             var that = this;
-            this.process_id = setTimeout(function () {
-                if ($('.is-deploy-view-id').length) {
-                    that.deployView();
-                }
-                that.runProcess();
-            }, 1500);
         },
         init: function () {
             this.runProcess();

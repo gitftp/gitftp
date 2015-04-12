e76695220ac7383511bd6da7bfcb071ac3ae585a
boniface pereira <bonifacepereira@gmail.com>
2015-04-12 11:20:57 +0530

    'update'

--- assets/js/dashboard/process.js
+++ assets/js/dashboard/process.js
@@ -42,7 +42,6 @@ define([
                         var timestamp = (new Date(parseInt($this.attr('data-timestamp'))*1000)).getTime();
                         var currtime = 
                         var diff = parseInt((new Date()).getTime().toString().substr(0, 10))-timestamp;
-                        var timeec = (new Date()).getTime()-timestamp;
                         $this.html(moment.duration(timeec).humanize()+' ago');
                     });
                     

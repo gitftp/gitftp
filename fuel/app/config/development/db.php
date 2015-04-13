cd081413a0fbb135b1a651f91e27ea7f893f92d9
boniface pereira <bonifacepereira@gmail.com>
2015-04-14 02:44:52 +0530

    'update'

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
     'default' => array(
         'connection' => array(
-            'dsn' => 'mysql:host=git.gitftp.com;dbname=gitftp',
+//            'dsn' => 'mysql:host=git.gitftp.com;dbname=gitftp',
             'hostname' => 'git.gitftp.com',
             'database' => 'gitftp',
             'username' => 'gitftp',

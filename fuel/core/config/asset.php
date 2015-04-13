e468737c64bf03a454e12f31cf0286f6a5f4af95
boniface pereira <bonifacepereira@gmail.com>
2015-04-14 00:37:33 +0530

    'update'

--- fuel/core/config/asset.php
+++ fuel/core/config/asset.php
@@ -32,7 +32,7 @@ return array(
 	 *
 	 * Paths specified here are suffixed with the sub-folder paths defined below.
 	 */
-	'paths' => array('assets/'),
+	'paths' => array('http://cdn.gitftp.com/assets/'),
 
 	/**
 	 * Asset Sub-folders

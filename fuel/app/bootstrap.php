2727d6bf2b81fdb80a8dabda31fe2804c605d5b5
Boniface <Boniface@boniface-pc>
2015-04-13 21:00:43 +0530

    amazon

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -28,6 +28,8 @@ Autoloader::register();
 
 if($_SERVER['HTTP_HOST'] == 'localhost'){
     Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : Fuel::DEVELOPMENT);
+}else if($_SERVER['HTTP_HOST'] == 'git.gitftp.com' || $_SERVER['HTTP_HOST'] == '54.149.18.148'){
+    Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : Fuel::AMAZON);
 }else{
     Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : Fuel::TEST);
 }
---
+++ fuel/app/config/amazon/db.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * The production database settings. These get merged with the global settings.
+ */
+return array(
+    'default' => array(
+        'connection' => array(
+            'dsn' => 'mysql:host=localhost;dbname=gitftp',
+            'username' => 'gitftp',
+            'password' => 'thisissparta',
+        ),
+    ),
+);

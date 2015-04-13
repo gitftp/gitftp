66643007f88d0c86d6989a8dc60301d4eb0c34a8
boniface pereira <bonifacepereira@gmail.com>
2015-04-14 01:40:35 +0530

    'update'

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -7,7 +7,7 @@ $host = $_SERVER['HTTP_HOST'];
 
 if(preg_match('/git.gitftp.com|stg.gitftp.com/i', $host)) {
     $is_dash = true;
-    $controller = 'dashboard/index';
+    $controller = 'welcome/index';
     $dash_url = $host;
     $home_url = ($host == 'git.gitftp.com') ? 'http://gitftp.com/': 'http://stg-home.gitftp.com/';
 }else{

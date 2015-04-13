8f3c8d97bf98b4434f35b7c56067d4caead0162d
boniface pereira <bonifacepereira@gmail.com>
2015-04-14 00:47:29 +0530

    'update'

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -12,16 +12,16 @@ if(preg_match('/git.gitftp.com|stg.gitftp.com/i', $host)) {
     $is_dash = false;
 }
 
-if('/localhost/' == $host){
+if('localhost' == $host){
     $base = 'http://stg.gitftp.com/';
 }
-if('/stg.gitftp.com/' == $host){
+if('stg.gitftp.com' == $host){
     $base = 'http://localhost/';
 }
-if('/git.gitftp.com/' == $host){
+if('git.gitftp.com' == $host){
     $base = 'http://gitftp.com/';
 }
-if('/gitftp.com/' == $host){
+if('gitftp.com' == $host){
     $base = 'http://git.gitftp.com/';
 }
 

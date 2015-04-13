cb0eb1e5d261d11b22e555b280eefc840c7a6156
boniface pereira <bonifacepereira@gmail.com>
2015-04-14 01:03:34 +0530

    'update'

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -4,12 +4,12 @@
  * Hosting configuration for homepage and dashboard application.
  */
 $host = $_SERVER['HTTP_HOST'];
+
 if(preg_match('/git.gitftp.com|stg.gitftp.com/i', $host)) {
     $is_dash = true;
     $controller = 'dashboard/index';
     $dash_url = 'http://stg.gitftp.com/';
     $home_url = 'http://localhost/gitploy';
-    
 }else{
     $controller = 'welcome/index';
     $is_dash = false;

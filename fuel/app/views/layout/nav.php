520340399ba600114e232cc03661788b5ece1583
boniface pereira <bonifacepereira@gmail.com>
2015-04-14 01:46:15 +0530

    'update'

--- fuel/app/views/layout/nav.php
+++ fuel/app/views/layout/nav.php
@@ -60,7 +60,7 @@
 
                             <form class="navbar-form navbar-right" method="POST" action="<?php echo dash_url ?>user/login" role="search">
                                 <div class="form-group">
-                                    <input type="text" name="email" class="form-control" placeholder="Email" value="<?php echo (!is_dash)? '' : '' ?>">
+                                    <input type="text" name="email" class="form-control" placeholder="Email" value="<?php echo (!is_dash)? 'bonifacepereira@gmail.com' : '' ?>">
                                 </div>
                                 <div class="form-group">
                                     <input type="text" name="password" class="form-control" placeholder="Password" value="">

49b2c1111b0228b812f82d0133110513968b0383
boniface pereira <bonifacepereira@gmail.com>
2015-04-14 01:54:32 +0530

    'update'

--- fuel/app/classes/controller/test.php
+++ fuel/app/classes/controller/test.php
@@ -9,7 +9,7 @@ class Controller_Test extends Controller {
         echo dash_url;
         echo '----';
         echo Auth::check();
-        Auth::login()
+        Auth::login('bonifacepereira@gmail.com', 'thisissparta');
     }
     public function action_index() {
         echo '<pre>';

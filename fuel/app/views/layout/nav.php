23a688603ccd0bf8fdfb02b9f626e4e09f5c3c55
boniface pereira <bonifacepereira@gmail.com>
2015-04-14 00:51:29 +0530

    'update'

--- fuel/app/views/layout/nav.php
+++ fuel/app/views/layout/nav.php
@@ -60,10 +60,10 @@
 
                             <form class="navbar-form navbar-right" method="POST" action="<?php echo Uri::base(false) ?>user/login" role="search">
                                 <div class="form-group">
-                                    <input type="text" name="email" class="form-control" placeholder="Email" value="bonifacepereira@gmail.com">
+                                    <input type="text" name="email" class="form-control" placeholder="Email" value="">
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" name="password" class="form-control" placeholder="Password" value="thisissparta">
+                                    <input type="text" name="password" class="form-control" placeholder="Password" value="">
                                 </div>
                                 <button type="submit" class="btn btn-danger">Login</button>
                             </form>

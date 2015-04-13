72875fcada9556d630faa8b0b1d17ebbc77a2189
boniface pereira <bonifacepereira@gmail.com>
2015-04-14 02:58:33 +0530

    'update'

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -14,7 +14,7 @@ if(preg_match('/git.gitftp.com|stg.gitftp.com/i', $host)) {
     $controller = 'dashboard/index';
     $is_dash = true;
     $dash_url = 'http://'.$host.'/';
-    $home_url = ($host == 'git.gitftp.com') ? 'http://gitftp.com/': 'http://stg-home.gitftp.com/';
+    $home_url = ($host == 'git.gitftp.com') ? 'http://www.gitftp.com/': 'http://stg-home.gitftp.com/';
 }else{
     $controller = 'welcome/index';
     $is_dash = false;

c448f8c56130ae3ab4d33b0a1ba8f3c744d5c29b
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 00:47:07 +0530

    'update'

--- fuel/packages/utils/classes/utils.php
+++ fuel/packages/utils/classes/utils.php
@@ -105,7 +105,6 @@ class utils {
         $si_prefix = array( 'B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB' );
         $base = 1024;
         $class = min((int)log($bytes , $base) , count($si_prefix) - 1);
-        echo $bytes . '<br />';
         echo sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
     }
 

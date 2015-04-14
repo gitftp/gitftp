ec00342ec34280df331da0afa2fe1d16353303c4
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 01:08:41 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -12,7 +12,9 @@ class Controller_Api_Deploy extends Controller {
         $a = shell_exec("du -hs $dir");
         $a = explode('	', $a);
         $disk_usage_human = $a[0];
-        
+        echo json_encode(array(
+            
+        ))
     }
     public function action_getonly($id = null) {
         $a = $_POST;

46d44cd01fa2094c8e3a6ea65a2221ee9e499217
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 00:58:33 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -9,7 +9,7 @@ class Controller_Api_Deploy extends Controller {
         $deploy = new Model_Deploy();
         $user_id = Auth::get_user_id()[1];
         $dir = DOCROOT.'fuel/repository/'.$user_id;
-        $a = shell_exec("du -hs $dir");
+        $a = shell_exec("du -s $dir");
         echo $a;
 //        echo utils::humanize_data($a);
     }

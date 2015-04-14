cea46296305cf45a57dc90e271a60bc2136f31d4
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 01:23:02 +0530

    'update'

--- assets/js/dashboard/views/dashboard.js
+++ assets/js/dashboard/views/dashboard.js
@@ -10,7 +10,7 @@ define([
                 method: 'get',
                 dataType: 'json',
             }).done(function(data){
-                that.$el.html();
+                that.$el.html(JSON.stringify(data));
             })
         },
     });

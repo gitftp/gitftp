b83aa62a1f6e7ca53e4726a2f8280d370da8edb8
boniface pereira <bonifacepereira@gmail.com>
2015-04-14 23:13:24 +0530

    'update'

--- assets/js/dashboard/views/ftpadd.js
+++ assets/js/dashboard/views/ftpadd.js
@@ -59,6 +59,7 @@ define([
                     title: (d.status) ? 'Successful' : 'Falied',
                     content: (d.status) ? 'Connection established successfully' : d.reason
                 });
+                $this.find('i').removeClass('fa-exchange').addClass('fa-spin fa-spinner').attr('disabled', true);
             });
         },
         oneline: function () {

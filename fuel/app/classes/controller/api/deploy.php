fda90140fc816a7b7e25cf5932f02c5d08ecde67
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 00:12:43 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -10,6 +10,7 @@ class Controller_Api_Deploy extends Controller {
         $a = $_POST;
         $a = explode(',', $a);
         $deploy = new Model_Deploy();
+        $deploy->get($id, $select)
     }
 
     public function action_getall($id = null) {

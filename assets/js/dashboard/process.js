4b05538e785e6989b134ab4dbdf7634f7529eeeb
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 00:07:59 +0530

    'update'

--- assets/js/dashboard/process.js
+++ assets/js/dashboard/process.js
@@ -25,6 +25,18 @@ define([
                 }
                 if($('.is-deploy-list-id').length){
                     
+                    var id = $('.is-deploy-view-id').attr('data-id');
+                    $.getJSON(base + 'api/deploy/getall/' + id, function (data) {
+                        data = data.data[0];
+                        var el = $('.project-v-status');
+                        if (data.status == 'Idle' || data.status == 'to be initialized') {
+                            el.removeClass("project-co-loading");
+                            el.html(data.status);
+                        } else {
+                            el.addClass("project-co-loading");
+                            el.html('<i class="fa fa-spin fa-refresh fa-fw"></i> ' + data.status);
+                        }
+                    });
                 }
                 that.deployView();
             }, 1500);

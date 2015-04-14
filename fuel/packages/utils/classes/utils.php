30afad9794394bb402e73c0375307e19b92e33ba
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 00:46:26 +0530

    'update'

--- fuel/packages/utils/classes/utils.php
+++ fuel/packages/utils/classes/utils.php
@@ -99,13 +99,14 @@ class utils {
         }
     }
     
-    public static humanize_data(){
+    public static humanize_data($data){
+        
         $bytes = disk_free_space("."); 
-    $si_prefix = array( 'B', 'KB', 'MB', 'GB', 'TB', 'EB', 'ZB', 'YB' );
-    $base = 1024;
-    $class = min((int)log($bytes , $base) , count($si_prefix) - 1);
-    echo $bytes . '<br />';
-    echo sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
+        $si_prefix = array( 'B', 'KB', 'MB', 'GB', 'TB', 'EB', 'ZB', 'YB' );
+        $base = 1024;
+        $class = min((int)log($bytes , $base) , count($si_prefix) - 1);
+        echo $bytes . '<br />';
+        echo sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
     }
 
 }

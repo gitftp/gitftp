bbc910af16494f593a74b5414087c552a78fe350
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 13:58:35 +0530

    'update'

--- fuel/app/views/page/home.php
+++ fuel/app/views/page/home.php
@@ -2,6 +2,7 @@
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
 
+    <?php echo View::forge('layout/nav'); ?>
 <div class="home-image">
     <?php echo View::forge('layout/nav'); ?>
 

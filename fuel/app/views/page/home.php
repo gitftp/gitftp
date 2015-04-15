8a080e54c3945ab51c816280a1f1f47858b4896f
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 14:04:24 +0530

    'update'

--- fuel/app/views/page/home.php
+++ fuel/app/views/page/home.php
@@ -14,7 +14,7 @@
                 <p class="text-center" style="color: #999">
                     <?php echo Asset::img('logo.png', array('class', 'logo-image')); ?> <br>
                     <span style="font-size: 3em">
-                        <span style="font-weight: 100">GIT and FTP together</span>
+                        <span style="font-weight: 100">GIT and FTP together perfectly</span>
                     </span>
                 </p>
             </div>

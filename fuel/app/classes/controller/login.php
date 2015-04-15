5ce582ebde220a4ac9c52ce77acfb6ada0c51035
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 23:03:07 +0530

    'update'

--- fuel/app/classes/controller/login.php
+++ fuel/app/classes/controller/login.php
@@ -9,7 +9,7 @@ class Controller_Login extends Controller {
         $view->js = View::forge('layout/js');
         $view->header = View::forge('layout/header');
         $view->footer = View::forge('layout/footer');
-        $view->body = View::forge('page/dashboard');
+        $view->body = View::forge('page/login');
         return $view;
     }
 

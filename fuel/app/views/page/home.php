c423be12b2cc48b1e903003e32b8e89c907bf67b
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 20:14:08 +0530

    'update'

--- fuel/app/views/page/home.php
+++ fuel/app/views/page/home.php
@@ -14,7 +14,7 @@
                     <?php echo Asset::img('logo-w.png', array('class', 'logo-image')); ?><br>
                     <span style="font-size: 3em;font-weight: 100; text-transform: lowercase;">Push &amp; Deploy</span>
                     <br>
-                    <a href="<?php echo Uri::base(false) ?>signup" class="btn btn-default">Get Started</a>
+                    <a href="<?php echo Uri::base(false) ?>signup" class="btn btn-default btn-lg">Get Started</a>
                 </p>
             </div>
         </div>

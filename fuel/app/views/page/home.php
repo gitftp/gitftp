8e1abd2bf87b2513e7f9a43194f47352ff5f4882
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 13:03:55 +0530

    'update'

--- fuel/app/views/page/home.php
+++ fuel/app/views/page/home.php
@@ -2,21 +2,21 @@
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
 
-<div class="home-image"></div>
+<div class="home-image">
+    <?php echo View::forge('layout/nav'); ?>
 
-<?php echo View::forge('layout/nav'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div style="height: 60px;"></div>
+                <p class="text-center" style="color: #999">
+                    <span style="font-size: 3em">
+                        <?php echo Asset::img('logo.png'); ?> <br>
+                        <span style="font-weight: 100">Set your workflow.</span>
+                    </span>
+                </p>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div style="height: 60px;"></div>
-            <p class="text-center" style="color: #999">
-                <span style="font-size: 3em">
-                    <?php echo Asset::img('logo.png'); ?> <br>
-                    <span style="font-weight: 100">Set your workflow.</span>
-                </span>
-            </p>
-               
+            </div>
         </div>
     </div>
 </div>

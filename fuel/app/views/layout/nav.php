a590be227e62d1d79ce0bd300d2c62abda1af1d2
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 22:57:57 +0530

    'update'

--- fuel/app/views/layout/nav.php
+++ fuel/app/views/layout/nav.php
@@ -70,7 +70,7 @@
                                     <input type="text" name="email" class="form-control" placeholder="Email" value="">
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" name="password" class="form-control" placeholder="Password" value="<?php echo (!is_dash)? 'thisissparta' : '' ?>">
+                                    <input type="text" name="password" class="form-control" placeholder="Password" value="">
                                 </div>
                                 <button type="submit" class="btn btn-danger">Login</button>
                             </form>

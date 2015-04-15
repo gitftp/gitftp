c415a5b3c75d210d7422865a09b60d0b14f84fb3
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 23:02:37 +0530

    'update'

---
+++ fuel/app/classes/controller/login.php
@@ -0,0 +1,20 @@
+<?php
+
+class Controller_Dashboard extends Controller {
+    
+    public function action_index() {
+            
+        if(!Auth::check() or !is_dash){
+            Response::redirect(home_url);
+        }
+        
+        $view = View::forge('layout/base_layout.mustache');
+        $view->css = View::forge('layout/css');
+        $view->js = View::forge('layout/js');
+        $view->header = View::forge('layout/header');
+        $view->footer = View::forge('layout/footer');
+        $view->body = View::forge('page/dashboard');
+        return $view;
+    }
+
+}

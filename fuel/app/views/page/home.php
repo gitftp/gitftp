131b4c1fe26cbc150a7f94e61b7addae6180bb3f
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 14:05:39 +0530

    'update'

--- fuel/app/views/page/home.php
+++ fuel/app/views/page/home.php
@@ -14,7 +14,7 @@
                 <p class="text-center" style="color: #999">
                     <?php echo Asset::img('logo.png', array('class', 'logo-image')); ?> <br>
                     <span style="font-size: 3em">
-                        <span style="font-weight: 100"></span>
+                        <span style="font-weight: 100">Push changes to GitHub or Bitbucket</span>
                     </span>
                 </p>
             </div>

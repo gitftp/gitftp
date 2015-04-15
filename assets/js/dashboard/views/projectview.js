8c9ea9b8f5e739c9a4b3793422244398dca4e2b2
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 11:13:32 +0530

    'update'

--- assets/js/dashboard/views/projectview.js
+++ assets/js/dashboard/views/projectview.js
@@ -117,7 +117,6 @@ define([
         render: function (id, which) {
             var that = this;
             $('<div class="projectview-wrapper bb-loading">').appendTo(this.$el);
-            return false;
             
             if (which == null) {
                 this.which = 'activity';

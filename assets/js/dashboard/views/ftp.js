cba8fa284b7aa05392d19e7ad7af6f2ea07a3115
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 11:23:59 +0530

    'update'

--- assets/js/dashboard/views/ftp.js
+++ assets/js/dashboard/views/ftp.js
@@ -17,7 +17,7 @@ define([
         },
         render: function (id) {
             var that = this;
-            this.el = $('<div class="ftpadd-wrapper bb-loading">').appendTo(this.$el);
+            this.el = $('<div class="ftplist-wrapper bb-loading">').appendTo(this.$el);
 
             $.getJSON(base + 'api/ftp/getall', function (data) {
                 var template = _.template(ftplist);

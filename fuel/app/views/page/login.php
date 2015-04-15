f4e46ce4526a4e71266626ad892449ff1f484ba4
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 23:27:47 +0530

    'update'

--- fuel/app/views/page/login.php
+++ fuel/app/views/page/login.php
@@ -14,7 +14,7 @@
                 	Login to <span class="blue">gitftp</span>.com
                 </h3>
                 <div class="space20"></div>
-                <form action="">
+                <form action="<?php echo  ?>">
                 	<div class="form-group">
                 		<label for="email">Email</label>
                 		<input type="email" id="email" name="email" placeholder="Your email" class="form-control input-lg">

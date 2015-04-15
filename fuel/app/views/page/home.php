68c42d3dced1860f28c1f5aba125260359e9cf8b
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 14:09:25 +0530

    'update'

--- fuel/app/views/page/home.php
+++ fuel/app/views/page/home.php
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div style="height: 40px;"></div>
-                <p class="text-center" style="color: #999">
+                <p class="text-center" style="color: #444">
                     <?php echo Asset::img('logo.png', array('class', 'logo-image')); ?> <br>
                     <span style="font-size: 3em; text-transform: uppercase;">Commit, Push, Deploy &amp; Yolo</span>
                 </p>

90c395204c5bc1cf410b3d4f9925b8db0f1fc20b
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 23:12:16 +0530

    'update'

--- fuel/app/views/page/login.php
+++ fuel/app/views/page/login.php
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
                 <h3 class="title text-center">
-                	Login to your a/c
+                	Login to gitftp
                 </h3>
                 <form action="">
                 	<div class="form-group">

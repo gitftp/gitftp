275652d95ce60d14ea166f2012f9ad42f667ce6e
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 22:58:23 +0530

    'update'

--- fuel/app/views/layout/nav.php
+++ fuel/app/views/layout/nav.php
@@ -65,17 +65,6 @@
                                 </li>
                             </ul>
 
-                            <form class="navbar-form navbar-right" method="POST" action="<?php echo dash_url ?>user/login" role="search">
-                                <div class="form-group">
-                                    <input type="text" name="email" class="form-control" placeholder="Email" value="">
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" name="password" class="form-control" placeholder="Password" value="">
-                                </div>
-                                <button type="submit" class="btn btn-danger">Login</button>
-                            </form>
-
-
                         <?php } ?>
 
                     </div><!-- /.navbar-collapse -->

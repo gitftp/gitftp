572ca45883a44dec600babcfc4548b6494b65e83
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 13:51:26 +0530

    'update'

--- fuel/app/views/page/home.php
+++ fuel/app/views/page/home.php
@@ -7,7 +7,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-8">
                 <div style="height: 60px;"></div>
                 <p class="text-center" style="color: #999">
                     <?php echo Asset::img('logo.png', array('class', 'logo-image')); ?> <br>
@@ -15,7 +15,6 @@
                         <span style="font-weight: 100">Set your workflow.</span>
                     </span>
                 </p>
-
             </div>
         </div>
     </div>

4cd94131a6fd0b56c014d893bdd89e7e138e82a1
boniface pereira <bonifacepereira@gmail.com>
2015-04-15 20:42:06 +0530

    'update'

--- fuel/app/classes/controller/test.php
+++ fuel/app/classes/controller/test.php
@@ -2,7 +2,7 @@
 
 class Controller_Test extends Controller {
     public function action_g(){
-       $email = "akshata.suvarna3@gmail.com";
+       $email = "megaurishrane3@gmail.com";
         $default = "http://www.somewhere.com/homestar.jpg";
         $size = 40;
         $grav_url = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;

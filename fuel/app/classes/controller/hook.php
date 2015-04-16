5b2fc365b88ecdb764687c23eb5a041478a64eb1
boniface pereira <bonifacepereira@gmail.com>
2015-04-16 18:28:12 +0530

    'update'

--- fuel/app/classes/controller/hook.php
+++ fuel/app/classes/controller/hook.php
@@ -14,6 +14,10 @@ class Controller_Hook extends Controller {
 
         $repo = DB::select()->from('deploy')->where('id', $deploy_id)->and_where('user_id', $user_id)
                         ->execute()->as_array();
+        
+        if($repo['ready'] == 0){
+            
+        }
 
         if (count($repo) == 0) {
             die('No such user or deploy found.');

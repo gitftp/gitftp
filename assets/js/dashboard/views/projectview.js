dbdf13dcf9e923ac3b482b5a58b69164843bdfad
boniface pereira <bonifacepereira@gmail.com>
2015-04-16 20:53:15 +0530

    'update'

--- assets/js/dashboard/views/projectview.js
+++ assets/js/dashboard/views/projectview.js
@@ -222,7 +222,7 @@ define([
                     'count': data.count,
                     'renderedCount': count+10
                 });
-                $this.parents('td').html('=================');
+                $this.parents('tr').html('=================').remove();
                 $('.deploy-sub-page table tbody').append(subPage);
             });
         },

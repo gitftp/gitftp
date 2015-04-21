84371b565eb72c4356b646dd6b09396a21f7299b
boniface pereira <bonifacepereira@gmail.com>
2015-04-21 22:17:46 +0530

    'update'

--- fuel/app/classes/controller/user.php
+++ fuel/app/classes/controller/user.php
@@ -64,7 +64,7 @@ class Controller_User extends Controller {
         } else {
             echo json_encode(array(
                 'status' => false,
-                'reason' => 'The form seems to have .',
+                'reason' => 'The form seems to have Invalid.',
                 'fields' => array(
                     'username' => $v->error('username') ? $v->error('username')->get_message() : null,
                     'password' => $v->error('password') ? $v->error('password')->get_message() : null,

0a8912b1764203bfb13b3af6ec8408310a0fd490
boniface pereira <bonifacepereira@gmail.com>
2015-04-21 22:21:42 +0530

    'update'

--- fuel/app/views/page/signup.php
+++ fuel/app/views/page/signup.php
@@ -19,7 +19,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="alert alert-danger">
-                                asdasd
+                                
                             </div>
                         </div>
                         <div class="col-md-6">

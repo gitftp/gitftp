8fc63a9c7c53765e43496e300d275cf6bce7e21d
boniface pereira <bonifacepereira@gmail.com>
2015-04-21 22:25:50 +0530

    'update'

--- fuel/app/views/page/signup.php
+++ fuel/app/views/page/signup.php
@@ -18,7 +18,7 @@
                 <form id="home-signup-module" action="<?php echo Uri::base(false) ?>user/login" method="POST">
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="alert alert-info hide signup-form-error"></div>
+                            <div class="alert alert-info signup-form-error" style="display:none"></div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">

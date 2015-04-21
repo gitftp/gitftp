062158449cb81e7925fe36b98697d8ceeec65c29
boniface pereira <bonifacepereira@gmail.com>
2015-04-21 22:15:17 +0530

    'update'

--- fuel/app/classes/controller/user.php
+++ fuel/app/classes/controller/user.php
@@ -69,6 +69,7 @@ class Controller_User extends Controller {
                     'username' => $v->error('username') ? $v->error('username')->get_message() : null,
                     'password' => $v->error('password') ? $v->error('password')->get_message() : null,
                     'fullname' => $v->error('fullname') ? $v->error('fullname')->get_message() : null,
+                    'email' => $v->error('email') ? $v->error('email')->get_message() : null,
                 )
             ));
         }

b915dde216811bf758b15199ff23bdde6cc3c7ea
boniface pereira <bonifacepereira@gmail.com>
2015-04-23 02:18:24 +0530

    'update'

--- fuel/packages/utils/classes/utils.php
+++ fuel/packages/utils/classes/utils.php
@@ -1,7 +1,16 @@
 <?php
 
 class utils {
-    
+    /**
+     * Get avatar of an email address.
+     * @param type $email
+     * @param type $s
+     * @param type $d
+     * @param type $r
+     * @param type $img
+     * @param type $atts
+     * @return string
+     */
     public static function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
         $url = 'http://www.gravatar.com/avatar/';
         $url .= md5( strtolower( trim( $email ) ) );

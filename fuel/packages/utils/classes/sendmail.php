a63bc9744e1a37cc0c2fca6b0baeb06b182d98e7
boniface pereira <bonifacepereira@gmail.com>
2015-04-24 02:01:11 +0530

    'update'

--- fuel/packages/utils/classes/sendmail.php
+++ fuel/packages/utils/classes/sendmail.php
@@ -7,16 +7,7 @@ class Sendmail {
         $url = home_url.'api/mail';
         $data = array('key1' => 'value1', 'key2' => 'value2');
 
-//        $email = Email::forge();
-//        $email->from('my@email.me', 'My Name');
-//        $email->to('receiver@elsewhere.co.uk', 'Johny Squid');
-//        $email->subject('This is the subject');
-//        $email->to(array(
-//            'example@mail.com',
-//            'another@mail.com' => 'With a Name',
-//        ));
-//        $email->body('This is my message');
-//        echo http_build_query($email);
+
         
         $options = array(
             'http' => array(
@@ -32,6 +23,15 @@ class Sendmail {
     }
     
     public static function sendMail(){
-        
+        //        $email = Email::forge();
+//        $email->from('my@email.me', 'My Name');
+//        $email->to('receiver@elsewhere.co.uk', 'Johny Squid');
+//        $email->subject('This is the subject');
+//        $email->to(array(
+//            'example@mail.com',
+//            'another@mail.com' => 'With a Name',
+//        ));
+//        $email->body('This is my message');
+//        echo http_build_query($email);
     }
 }

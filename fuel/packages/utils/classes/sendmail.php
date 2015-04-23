3f6dfb87217fb3735a3633a1b047293ba7add00c
boniface pereira <bonifacepereira@gmail.com>
2015-04-24 01:59:01 +0530

    'update'

--- fuel/packages/utils/classes/sendmail.php
+++ fuel/packages/utils/classes/sendmail.php
@@ -25,7 +25,7 @@ class Sendmail {
             'http' => array(
                 'header' => "Content-type: application/x-www-form-urlencoded\r\n",
                 'method' => 'POST',
-                'content' => http_build_query($email),
+                'content' => http_build_query($data),
             ),
         );
         $context = stream_context_create($options);

a7d1bc1f755d0721c783f5aa80b60f0f46c280e0
boniface pereira <bonifacepereira@gmail.com>
2015-04-24 01:52:29 +0530

    'update'

--- fuel/packages/utils/classes/sendmail.php
+++ fuel/packages/utils/classes/sendmail.php
@@ -5,7 +5,7 @@ class Sendmail {
     public static function send() {
         echo 'asd';
 
-        $url = 'http://server.com/path';
+        $url = home_url;
         $data = array('key1' => 'value1', 'key2' => 'value2');
 
         // use ksey 'http' even if you send the request to https://...

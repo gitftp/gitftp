b4568e43ca0293d342a398a2a08915abc8e7f4f7
boniface pereira <bonifacepereira@gmail.com>
2015-04-24 01:52:11 +0530

    'update'

--- fuel/packages/utils/classes/sendmail.php
+++ fuel/packages/utils/classes/sendmail.php
@@ -1,26 +1,25 @@
 <?php
 
 class Sendmail {
-    
+
     public static function send() {
         echo 'asd';
-        
+
         $url = 'http://server.com/path';
-$data = array('key1' => 'value1', 'key2' => 'value2');
+        $data = array('key1' => 'value1', 'key2' => 'value2');
 
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data),
-    ),
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
+        // use ksey 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method' => 'POST',
+                'content' => http_build_query($data),
+            ),
+        );
+        $context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
 
-var_dump($result);
-        
+        var_dump($result);
     }
-    
+
 }

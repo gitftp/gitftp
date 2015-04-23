121152ad503cd5d26c6c1965acb14c43970e8a66
boniface pereira <bonifacepereira@gmail.com>
2015-04-24 01:49:59 +0530

    'update'

--- fuel/packages/utils/classes/sendmail.php
+++ fuel/packages/utils/classes/sendmail.php
@@ -1,5 +1,16 @@
 <?php
 
 class Sendmail {
-    
+        public static function gitGetBranches($repo){
+        exec("git ls-remote --heads $repo", $op);
+        if(empty($op))
+            return false;
+        
+        foreach ($op as $k => $v) {
+            $b = preg_split('/\s+/', $v);
+            $b = explode('/', $b[1]);
+            $op[$k] = $b[2];
+        }
+        return $op;
+    }
 }

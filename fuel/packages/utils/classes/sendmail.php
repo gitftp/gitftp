25a0932ca4c8f2597e845fb69cc4da0cd13eeebb
boniface pereira <bonifacepereira@gmail.com>
2015-04-24 01:52:56 +0530

    'update'

--- fuel/packages/utils/classes/sendmail.php
+++ fuel/packages/utils/classes/sendmail.php
@@ -5,7 +5,7 @@ class Sendmail {
     public static function send() {
         echo 'asd';
 
-        $url = 'http://www.gitftp.com/api/mail/send';
+        $url = 'http://www.gitftp.com/api/mail';
         $data = array('key1' => 'value1', 'key2' => 'value2');
 
         // use ksey 'http' even if you send the request to https://...

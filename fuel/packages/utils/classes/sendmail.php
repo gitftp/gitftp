877d90610171d6f2766c301c0864c1bd93d36d4c
boniface pereira <bonifacepereira@gmail.com>
2015-04-24 13:07:14 +0530

    'update'

--- fuel/packages/utils/classes/sendmail.php
+++ fuel/packages/utils/classes/sendmail.php
@@ -1,7 +1,7 @@
 <?php
 
 class Sendmail {
-    
+
     /**
      * $data = array(
      *      'to' => $user_id,
@@ -10,8 +10,26 @@ class Sendmail {
      * @param array $data
      */
     public function send() {
-        
-        
+        $email = Email::forge();
+
+// Set the from address
+        $email->from('my@email.me', 'My Name');
+
+// Set the to address
+        $email->to('receiver@elsewhere.co.uk', 'Johny Squid');
+
+// Set a subject
+        $email->subject('This is the subject');
+
+// Set multiple to addresses
+
+        $email->to(array(
+            'example@mail.com',
+            'another@mail.com' => 'With a Name',
+        ));
+
+// And set the body.
+        $email->body('This is my message');
     }
 
 }

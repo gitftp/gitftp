f1a40afebe4ea25ccca6217a4969ff05157136e3
boniface pereira <bonifacepereira@gmail.com>
2015-04-24 14:55:06 +0530

    'update'

--- fuel/packages/utils/classes/sendmail.php
+++ fuel/packages/utils/classes/sendmail.php
@@ -13,8 +13,8 @@ class Sendmail {
         
         if($this->toUser == 0){
             $this->toUser = Auth::get_user_id()[1];
+            
         }
-        
         
         
         $email->to('bonifacepereira@outlook.com', 'Boniface Pereira');

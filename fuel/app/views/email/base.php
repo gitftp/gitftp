7bcb7b51f66980d02372644c53e180dcd4b67f46
boniface pereira <bonifacepereira@gmail.com>
2015-04-24 14:46:44 +0530

    'update'

--- fuel/app/views/email/base.php
+++ fuel/app/views/email/base.php
@@ -39,38 +39,7 @@
                                                     <td align="left" style=
                                                     "font-size:14px;color:#444444;font-family:'Open Sans','Lucida Grande','Segoe UI',Arial,Verdana,'Lucida Sans Unicode',Tahoma,'Sans Serif';border-collapse:collapse"
                                                     valign="top" width="454">
-                                                        Hi Boniface,<br>
-                                                        <br>
-                                                        <a href=
-                                                        "mailto:megaurishrane@gmail.com"
-                                                        target=
-                                                        "_blank">megaurishrane@gmail.com</a>
-                                                        installed <span class=
-                                                        "il">Dropbox</span>
-                                                        after getting your
-                                                        invite. To thank you,
-                                                        we've awarded you 500
-                                                        MB of bonus space. You
-                                                        now have 4.38 GB on
-                                                        <span class=
-                                                        "il">Dropbox</span>!<br>
-
-                                                        <br>
-                                                        Thanks!<br>
-                                                        - The <span class=
-                                                        "il">Dropbox</span>
-                                                        Team<br>
-                                                        <br>
-                                                        P.S. To get even more
-                                                        space, <a href=
-                                                        "https://www.dropbox.com/l/J182za4ptv00xnEs4oc5Gl/referrals"
-                                                        target="_blank">invite
-                                                        your friends</a> or
-                                                        <a href=
-                                                        "https://www.dropbox.com/l/BRYUNY05SvqeRTy6Oaa1Td/pricing"
-                                                        target="_blank">upgrade
-                                                        your <span class=
-                                                        "il">Dropbox</span></a>!
+                                                        <?php echo $content; ?>
                                                     </td>
 
                                                     <td width="36"></td>

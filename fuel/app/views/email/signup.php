a377d4281b1db488f25fc03b891726a59f428a9f
boniface pereira <bonifacepereira@gmail.com>
2015-04-24 14:47:46 +0530

    'update'

--- fuel/app/views/email/signup.php
+++ fuel/app/views/email/signup.php
@@ -1,8 +1,32 @@
-<?php
+Hi Boniface,<br>
+<br>
+<a href=
+   "mailto:megaurishrane@gmail.com"
+   target=
+   "_blank">megaurishrane@gmail.com</a>
+installed <span class=
+                "il">Dropbox</span>
+after getting your
+invite. To thank you,
+we've awarded you 500
+MB of bonus space. You
+now have 4.38 GB on
+<span class=
+      "il">Dropbox</span>!<br>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<br>
+Thanks!<br>
+- The <span class=
+            "il">Dropbox</span>
+Team<br>
+<br>
+P.S. To get even more
+space, <a href=
+          "https://www.dropbox.com/l/J182za4ptv00xnEs4oc5Gl/referrals"
+          target="_blank">invite
+    your friends</a> or
+<a href=
+   "https://www.dropbox.com/l/BRYUNY05SvqeRTy6Oaa1Td/pricing"
+   target="_blank">upgrade
+    your <span class=
+               "il">Dropbox</span></a>!

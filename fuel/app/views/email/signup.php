66c4042325020f4e4f701b827a5316e6eeb1d72b
boniface pereira <bonifacepereira@gmail.com>
2015-04-24 14:01:40 +0530

    'update'

--- fuel/app/views/email/signup.php
+++ fuel/app/views/email/signup.php
@@ -30,8 +30,7 @@
                                                 <td width="36"></td>
                                                 
                                                 <td align="left" valign="top" width="454" style=
-                                                    "font-size:14px;color:#444444;font-family:'Open Sans','Lucida Grande','Segoe UI',Arial,Verdana,'Lucida Sans Unicode',Tahoma,'Sans Serif';border-collapse:collapse"
-                                                    >
+                                                    "font-size:14px;color:#444444;font-family:'Open Sans','Lucida Grande','Segoe UI',Arial,Verdana,'Lucida Sans Unicode',Tahoma,'Sans Serif';border-collapse:collapse">
                                                     <div>
                                                     Hi Boniface,<br>
                                                     <br>

2df223f0085feb7ee05b84be4d66409ae31e0567
boniface pereira <bonifacepereira@gmail.com>
2015-04-26 16:12:33 +0530

    'update'

--- assets/js/dashboard/views/projectadd.js
+++ assets/js/dashboard/views/projectadd.js
@@ -9,7 +9,7 @@ define([
             'keyup input#add-repo': 'calcname',
             'change #deploy-add-privaterepo': 'priCheck',
             'click .testconnectiontorepo': 'testConnectionToRepo',
-            'click button[name=""]'
+            'click .add-repo-addmoreenv': ''
         },
         testConnectionToRepo: function (e) {
 

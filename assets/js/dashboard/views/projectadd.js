7b3c3a38863b76869dddcd8d97775090827c2329
boniface pereira <bonifacepereira@gmail.com>
2015-04-26 16:35:58 +0530

    'update'

--- assets/js/dashboard/views/projectadd.js
+++ assets/js/dashboard/views/projectadd.js
@@ -86,9 +86,9 @@ define([
             $this.attr('readonly', true);
             $('#add-deploy-form').find('select,input').attr('readonly', true);
             
-            var env = {};
             
             $.each($('.env-rows'), function(i, a){
+                var env = {};
                 
             });
             

c4b5ac40212a5c7c37b7947edfffd9d850176a14
boniface pereira <bonifacepereira@gmail.com>
2015-04-26 14:06:59 +0530

    'update'

--- assets/js/dashboard/views/projectadd.js
+++ assets/js/dashboard/views/projectadd.js
@@ -53,7 +53,7 @@ define([
         },
         calcname: function (e) {
             
-            .addClass('btn-success').html('<i class="fa fa-check"></i> connected').prop('disabled', false);
+            .removeClass('btn-success').html('<i class="fa fa-check"></i> connected').prop('disabled', false);
             
             var $this = $(e.currentTarget);
             var str = $this.val();

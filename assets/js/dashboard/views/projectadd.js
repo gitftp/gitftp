ae1dc201bf4f968ad9d42cff10b1a1c991f7b279
boniface pereira <bonifacepereira@gmail.com>
2015-04-26 14:03:15 +0530

    'update'

--- assets/js/dashboard/views/projectadd.js
+++ assets/js/dashboard/views/projectadd.js
@@ -18,6 +18,7 @@ define([
             var $this = $(e.currentTarget);
             var that = this;
             $this.attr('data-html',$this.html()).html('<i class="fa fa-spin fa-spinner"></i>').prop('disabled', true);
+            
             _ajax({
                 url: base + 'api/deploy/getbranches',
                 data: {

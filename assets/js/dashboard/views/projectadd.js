e9b39b6861d0fd8f6b176a4f5c15afa049332c77
boniface pereira <bonifacepereira@gmail.com>
2015-04-26 16:43:13 +0530

    'update'

--- assets/js/dashboard/views/projectadd.js
+++ assets/js/dashboard/views/projectadd.js
@@ -92,6 +92,7 @@ define([
                 
                 envprop = {
                     'env_name' : b.find('input[name="env_name"]').val(),
+                    'env_name' : b.find('input[name="env_branch"]').val(),
                 }
                 
             });

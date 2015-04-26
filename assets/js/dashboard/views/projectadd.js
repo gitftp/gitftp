98aa4c8faef9c429bcdeed14092cf1fc2b5fd719
boniface pereira <bonifacepereira@gmail.com>
2015-04-26 14:04:03 +0530

    'update'

--- assets/js/dashboard/views/projectadd.js
+++ assets/js/dashboard/views/projectadd.js
@@ -29,7 +29,7 @@ define([
                 console.log(data);
                 if(data.status){
                     that._branches = data.data;
-                    
+                    $this.addClass('btn-success').
                 }else{
                     $.alert({
                         title: 'Something went wrong.',

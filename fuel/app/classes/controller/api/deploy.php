1c5fbea04b3f3e925f2c34f873cb871505f122d4
boniface pereira <bonifacepereira@gmail.com>
2015-04-27 20:26:33 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -198,7 +198,7 @@ class Controller_Api_Deploy extends Controller {
             echo json_encode(array(
                 'status' => false,
                 'request' => $i,
-                
+                'reason' => 'Failed to write to database.'
             ));
             
         }

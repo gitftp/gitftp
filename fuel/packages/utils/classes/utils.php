636141d301f437521987f402e862099dae37b14d
boniface pereira <bonifacepereira@gmail.com>
2015-04-28 21:41:47 +0530

    'update'

--- fuel/packages/utils/classes/utils.php
+++ fuel/packages/utils/classes/utils.php
@@ -5,7 +5,7 @@ class utils {
      * Executes an Git command and returns the results.
      * @param type $arg
      */
-    public static function gitGetBranches($repo){
+    public static function gitGetBranches($repo, $username, $password){
         if(trim($repo) == ''){
             return false;
         }

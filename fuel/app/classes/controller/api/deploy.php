0899050c394b87f216b596f6e8444773c8ac57ef
boniface pereira <bonifacepereira@gmail.com>
2015-04-28 21:47:48 +0530

    'update'

--- fuel/app/classes/controller/api/deploy.php
+++ fuel/app/classes/controller/api/deploy.php
@@ -101,48 +101,9 @@ class Controller_Api_Deploy extends Controller {
             ));
             return;
         }
-
-        $user_id = Auth::get_user_id()[1];
-        $b = DB::select()->from('deploy')->where('id', $id)->and_where('user_id', $user_id)
-                        ->execute()->as_array();
-
-        $status = strtolower($b[0]['status']);
-
-        if ($status == 'idle' || $status == 'to be initialized') {
-
-            $user_dir = DOCROOT . 'fuel/repository/' . $user_id;
-            $repo_dir = DOCROOT . 'fuel/repository/' . $user_id . '/' . $b[0]['id'];
-
-            try {
-                chdir($repo_dir);
-                echo shell_exec('chown www-data * -R');
-                echo shell_exec('chgrp www-data * -R');
-                echo shell_exec('chmod 777 -R');
-                File::delete_dir($repo_dir, true, true);
-            } catch (Exception $ex) {
-                
-            }
-
-            if (count($b) != 0) {
-                DB::delete('deploy')->where('id', $id)->execute();
-                echo json_encode(array(
-                    'status' => true,
-                    'request' => $id,
-                ));
-            } else {
-                echo json_encode(array(
-                    'status' => false,
-                    'request' => $id,
-                    'reason' => 'No access'
-                ));
-            }
-        } else {
-            return json_encode(array(
-                'status' => false,
-                'reason' => 'deploy busy, unable to delete in between of work',
-                'request' => $id
-            ));
-        }
+        
+        
+        
     }
 
     public function action_new() {

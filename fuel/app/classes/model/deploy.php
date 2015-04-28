40a623041b57a9f98dc299a4685d5a0b3805b709
boniface pereira <bonifacepereira@gmail.com>
2015-04-28 21:35:46 +0530

    'update'

--- fuel/app/classes/model/deploy.php
+++ fuel/app/classes/model/deploy.php
@@ -35,7 +35,7 @@ class Model_Deploy extends Model {
             if (isset($v['ftp'])) {
                
                 $ub = unserialize($v['ftp']);
-                $a[$k]['ftp'] = 
+                $a[$k]['ftp'] = $ub;
 //                $a[$k]['ftpdata'] = unserialize($a[$k]['ftp']);
 //                $a[$k]['ftp'] = $c;
 //            }

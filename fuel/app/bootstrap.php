271e5f3b30cc519632b3a79b4be9c18349b2fabb
Boniface Pereira <bonifacepereira@gmail.com>
2015-07-20 20:12:36 +0530

    update fuel/app/bootstrap.php

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -1,46 +1,41 @@
 <?php
-
-// Load in the Autoloader
-require COREPATH . 'classes' . DIRECTORY_SEPARATOR . 'autoloader.php';
-class_alias('Fuel\\Core\\Autoloader', 'Autoloader');
-
 // Bootstrap the framework DO NOT edit this
-require COREPATH . 'bootstrap.php';
+require COREPATH.'bootstrap.php';
 
-
-Autoloader::add_classes(array(
-        // Add classes you want to override here
-        // Example: 'View' => APPPATH.'classes/view.php',
+\Autoloader::add_classes(array(
+	// Add classes you want to override here
+	// Example: 'View' => APPPATH.'classes/view.php',
 ));
 
 // Register the autoloader
-Autoloader::register();
+\Autoloader::register();
 
 /**
  * Your environment.  Can be set to any of the following:
  *
  * Fuel::DEVELOPMENT
  * Fuel::TEST
-* Fuel::STAGE
+ * Fuel::STAGING
  * Fuel::PRODUCTION
  */
+// \Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : \Fuel::DEVELOPMENT);
 
-$producion = array(
-    'console.gitftp.com',
-    'www.gitftp.com',
-    'gitftp.com',
-    '54.149.18.148'
-);
-$development = array(
-    'stg.gitftp.com',
-    'stg-home.gitftp.com',
-);
-$staging = array(
+//$producion = array(
+//    'console.gitftp.com',
+//    'www.gitftp.com',
+//    'gitftp.com',
+//    '54.149.18.148'
+//);
+//$development = array(
+//    'stg.gitftp.com',
+//    'stg-home.gitftp.com',
+//);
+//$staging = array(
+//
+//);
 
-);
-
-Fuel::$env = Fuel::DEVELOPMENT;
+\Fuel::$env = \Fuel::DEVELOPMENT;
 
 // Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : Fuel::DEVELOPMENT);
 // Initialize the framework with the config file.
-Fuel::init('config.php');
+\Fuel::init('config.php');

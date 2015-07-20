e3c9adaafce44e359da47592bb378b244c7b6e0a
Boniface Pereira <bonifacepereira@gmail.com>
2015-07-20 20:09:09 +0530

    core update lang

--- fuel/core/lang/en/test.php
+++ fuel/core/lang/en/test.php
@@ -3,15 +3,13 @@
  * Part of the Fuel framework.
  *
  * @package    Fuel
- * @version    1.5
+ * @version    1.7
  * @author     Fuel Development Team
  * @license    MIT License
- * @copyright  2010 - 2013 Fuel Development Team
+ * @copyright  2010 - 2015 Fuel Development Team
  * @link       http://fuelphp.com
  */
 
-
 return array(
-	'hello' => 'Hello there :name!'
+	'hello' => 'Hello there :name!',
 );
-

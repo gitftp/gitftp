a3c943d326914f8a418e5f901ef2e7dbe59f6126
Boniface Pereira <bonifacepereira@gmail.com>
2015-07-22 00:25:51 +0530

    development env -> local

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
     'default' => array(
         'connection' => array(
-            'dsn'      => 'mysql:host=localhost;dbname=craftrzt_gitploy',
+            'dsn'      => 'mysql:host=localhost;dbname=craftrzt_gitftp',
             'username' => 'root',
             'password' => '',
         ),

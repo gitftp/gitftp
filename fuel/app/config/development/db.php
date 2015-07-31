8ce739205bda5be674093a2f7268e018d938d63b
Boniface Pereira <bonifacepereira@gmail.com>
2015-07-31 15:58:14 +0530

    local db no longer used for development,

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -3,12 +3,22 @@
  * The development database settings. These get merged with the global settings.
  */
 
+//return array(
+//    'default' => array(
+//        'connection' => array(
+//            'dsn'      => 'mysql:host=192.168.1.12;dbname=craftrzt_gitftp',
+//            'username' => 'root',
+//            'password' => '',
+//        ),
+//    ),
+//);
+
 return array(
     'default' => array(
         'connection' => array(
-            'dsn'      => 'mysql:host=localhost;dbname=craftrzt_gitftp',
-            'username' => 'root',
-            'password' => '',
+            'dsn'      => 'mysql:host=craftpip.com;dbname=craftrzt_gitftp',
+            'username' => 'craftrzt_gitftp',
+            'password' => 's.AmN-V?rXCT',
         ),
     ),
 );

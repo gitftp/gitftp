755f435dfa9d884b15c298c3e5230b8924450d29
Boniface Pereira <bonifacepereira@gmail.com>
2015-08-01 18:36:03 +0530

    imp admin fix

--- fuel/app/classes/controller/administrator/admincheck.php
+++ fuel/app/classes/controller/administrator/admincheck.php
@@ -3,7 +3,7 @@
 class Controller_Administrator_Admincheck extends Controller_Rest {
     public function before() {
         parent::before();
-        if (!\Auth::instance()->check() && \Auth::instance()->get('group') !== 2) {
+        if (!\Auth::instance()->check() || \Auth::instance()->get('group') !== 2) {
             Response::redirect(home_url . 'login#');
         }
     }

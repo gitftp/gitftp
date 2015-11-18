b78d5c0f2a20759452fc22af4436131f7b7f6444
Boniface Pereira <bonifacepereira@gmail.com>
2015-11-18 18:19:19 +0530

    UPDATE

--- fuel/app/classes/controller/api/apilogincheck.php
+++ fuel/app/classes/controller/api/apilogincheck.php
@@ -3,7 +3,7 @@
 class Controller_Api_Apilogincheck extends Controller_Rest {
     public function before() {
         parent::before();
-        if (!\Auth::instance()->check() && false) {
+        if (!\Auth::instance()->check()) {
             echo json_encode(array(
                 'status' => FALSE,
                 'reason' => '10001'
